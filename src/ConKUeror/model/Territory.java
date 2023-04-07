700afd26e473e8dd9c1b160e2cbe813cacfd018a
batu <baltinok19@ku.edu.tr>
2023-04-07 12:31:38 +0300

    addArmy and RemoveArmy committed

--- src/ConKUeror/model/Territory.java
+++ src/ConKUeror/model/Territory.java
@@ -1,5 +1,24 @@
 package src.ConKUeror.model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Territory {
     
+    private String name;
+    private List<Array> armies;
+
+    public Territory(String name) {
+        this.name = name;
+        this.armies = new ArrayList<>();
+    }
+
+    public void addArmy(Army army) {
+        // TODO: Implement army addition logic
+    }
+
+    public void removeArmy(Army army) {
+        // TODO: Implement army removal logic
+    }
 }
