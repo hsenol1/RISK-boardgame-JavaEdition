c37717fe8a9d57e745496c95674e51ec485dec7d
batu <baltinok19@ku.edu.tr>
2023-04-17 23:04:51 +0300

    get Cards and Armies added on player

--- src/ConKUeror/model/Player.java
+++ src/ConKUeror/model/Player.java
@@ -8,16 +8,24 @@ public class Player {
         private String name;
         private List<Army> armies;
         private List<Territory> territories;
-        //private List<Card> cards;
+        private List<Card> cards;
 
-        public Player(String name) {
+        public Player(String name, List<Card> cards) {
             this.name = name;
+            this.cards = cards;
+
         }
 
         public String getName() {
 
             return this.name;
         }
+        
+        public List<Card> getCards() {
+
+            return this.cards;
+        }
+
         
         
     }
