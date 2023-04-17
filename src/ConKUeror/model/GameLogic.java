5e5596e2ba04683a03c2ffe8e2ceec0038b5ded9
batu <baltinok19@ku.edu.tr>
2023-04-17 23:13:09 +0300

    TradeCardArmy logic implemented

--- src/ConKUeror/model/GameLogic.java
+++ src/ConKUeror/model/GameLogic.java
@@ -1,9 +1,18 @@
 package src.ConKUeror.model;
 
 public class GameLogic {
+
+    private PlayerInventory p;
+    
+    public void tradeCardArmy(Card c[], Army a[]) {
+
+  p.deleteCards(c);
+  p.insertArmies(a);
+
+
+    }
     
 
 
 
-    
 }
