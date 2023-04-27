1647db9ab4fd382dd84ac5df67596008032699ef
Batuhan Arat <arat_batuhan@windowslive.com>
2023-04-27 21:40:36 +0300

    UI is enhanced

--- src/ConKUeror/UI/PlayerPanel.java
+++ src/ConKUeror/UI/PlayerPanel.java
@@ -2,12 +2,11 @@ package src.ConKUeror.UI;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.List;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
@@ -21,65 +20,70 @@ public class PlayerPanel extends JPanel{
     private ButtonHandler buttonHandler;
 
     private int playerCount;
-    private int eachPlayerInfoWidth = 150;
-    private int playerInfoHeight = 30;
     private List<Player> orderedPlayers;
+
+    Border blackBorder;
+    Border padding;
+    int panelWidth;
+    int panelHeight;
 
 
     public PlayerPanel(ButtonHandler buttonHandler) {
         this.buttonHandler = buttonHandler;
+
         playerCount = StartMode.getOrderedPlayerList().size();
-       setLayout(new GridLayout(1, playerCount));
+        setLayout(new GridLayout(1, playerCount));
+        orderedPlayers=StartMode.getOrderedPlayerList();
 
+        setUI();
+        setPlayerInfos();
+   }
 
-        //setBounds(540 ,5 ,eachPlayerInfoWidth*playerCount, playerInfoHeight);
-        //setPreferredSize(new Dimension(eachPlayerInfoWidth * playerCount, playerInfoHeight));
+   public void setUI() {
 
-         orderedPlayers=buttonHandler.getBuildMode().getPlayers();
+     blackBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+     padding = BorderFactory.createEmptyBorder(5, 5, 5, 5);
 
+        int screenWidth = 1115;
+        int availableWidth = screenWidth - 450;
+        panelWidth = availableWidth / playerCount;
+        panelHeight = 30;
 
-         Border blackBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
-         Border padding = BorderFactory.createEmptyBorder(5, 5, 5, 5);
-
-         int screenWidth = 1115;
-         int screenHeight = 770;
- 
-         // Calculate the panel dimensions
-         //int maxPanelWidth = screenWidth / 3;
-         //int minPanelWidth = screenWidth / 6;
-         //int panelWidth = Math.min(maxPanelWidth, screenWidth / playerCount);
-         //panelWidth = Math.max(minPanelWidth, panelWidth);
-         int availableWidth = screenWidth - 450;
-        int panelWidth = availableWidth / playerCount;
-         int panelHeight = 30;
-
-         int startX = 460;
+        int startX = 460;
         int startY = 5;
 
         setBounds(startX, startY, panelWidth * playerCount, panelHeight);
         setPreferredSize(new Dimension(panelWidth * playerCount, panelHeight));
 
-         for (Player p : orderedPlayers){
+   }
+   
+
+   public void setPlayerInfos() {
+        for (Player p : orderedPlayers){
             JPanel playerInfoPanel = new JPanel();
 
         playerInfoPanel.setBorder(BorderFactory.createCompoundBorder(blackBorder, padding));
-        playerInfoPanel.setBackground(Color.gray);
+        playerInfoPanel.setBackground(Color.lightGray);
         JLabel playerNameLabel = new JLabel(p.getName() + ":");
-        JLabel armyCountLabel = new JLabel(Integer.toString(p.getInventory().getTotalArmy()));
+        String army = Integer.toString(p.getInventory().getTotalArmy());
+        System.out.println(army);
+        JLabel armyCountLabel = new JLabel(army);
+        Font labelFont = new Font("Arial", Font.PLAIN, 12);
+        playerNameLabel.setFont(labelFont);
+        armyCountLabel.setFont(labelFont);
+
         
         playerInfoPanel.add(playerNameLabel);
         playerInfoPanel.add(armyCountLabel);
-       
+
+    
         playerInfoPanel.setPreferredSize(new Dimension(panelWidth, panelHeight));
-       
+    
         add(playerInfoPanel);
         
-        //add(playerNameLabel);
-        //add(armyCountLabel);
+    
     }
-
-
-   }
+}
 
 
 
