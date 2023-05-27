6d0aff7e0b77ec49a0598f51235a34fde0e14158
Batuhan Arat <arat_batuhan@windowslive.com>
2023-05-27 03:35:59 +0300

    computer deploy with player panel bug

--- src/ConKUeror/domain/model/Player/Strategies/ComputerPlayer/ComputerPlayerDeploy.java
+++ src/ConKUeror/domain/model/Player/Strategies/ComputerPlayer/ComputerPlayerDeploy.java
@@ -1,14 +1,61 @@
 package ConKUeror.domain.model.Player.Strategies.ComputerPlayer;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+import ConKUeror.domain.controller.HandlerFactory;
+import ConKUeror.domain.controller.MapHandler;
 import ConKUeror.domain.model.Board.Territory;
+import ConKUeror.domain.model.Player.Player;
+import ConKUeror.domain.model.Player.PlayerExpert;
 import ConKUeror.domain.model.Player.Strategies.IDeployBehaviour;
 
 public class ComputerPlayerDeploy implements IDeployBehaviour{
 
+
+    public void waitAndExecute(Runnable action) {
+        int delay = 1000; // delay for 1 second.
+        Timer timer = new Timer(delay, null);
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                SwingUtilities.invokeLater(action);
+                timer.stop(); // stop the timer after it fires
+            }
+        };
+        timer.addActionListener(taskPerformer);
+        timer.start();
+    }
+
+
+
     @Override
     public void deploy(Territory t, int army) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deploy'");
+
+
+        if (t == null) {
+            return; }
+
+        t.addInfantries(army);
+        Player player = PlayerExpert.getPlayerInTurn();
+        int player_index =PlayerExpert.getPlayersList().indexOf(player);
+
+        player.getInventory().removeInfantries(army);
+
+        PlayerExpert.updatePlayerCount(player_index);
+
+        MapHandler mapHandler =  HandlerFactory.getInstance().giveMapHandler();
+
+        int buttonId = t.getId();
+        int result = t.getTotalUnit();
+
+        waitAndExecute(() -> {
+            mapHandler.updateTerritory(buttonId,result);
+        });
+
     }
 
 
